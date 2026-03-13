dfdb267a3ce3df99012e6e9f4cfcf33322f65288
Angela Murray <angelamurray776@outlook.com>
2026-03-12 22:11:20 -0400

    fix render

--- routes/characters.js
+++ routes/characters.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Character = require('../models/character');
+const Character = require('../models/Character');
 
 // GET all characters
 router.get('/', async (req, res) => {
