5214107a1b363332ec185cc09b66884525eca512
Angela Murray <angelamurray776@outlook.com>
2026-03-15 01:21:33 -0400

    another fix with swagger.js

--- swagger.js
+++ swagger.js
@@ -5,7 +5,14 @@ const doc = {
     title: 'RPG Tracker API',
     description: 'API documentation for the RPG Tracker project'
   },
-  host: 'localhost:3000',
+      servers: [
+      {
+        url: "http://localhost:3000"
+      },
+      {
+        url: 'https://rpgtracker.onrender.com/',
+      }
+    ],
 schemes: ['http'],
 };
 
@@ -16,3 +23,44 @@ const routes = ['./routes/index.js'];
 swaggerAutogen(outputFile, routes, doc);
 
 // "npm run swagger" will rebuild the swagger-output file.
+
+
+
+// const express = require("express");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerJsdoc = require("swagger-jsdoc");
+
+// const app = express();
+// app.use(express.json());
+
+// const swaggerOptions = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "RPG Tracker API",
+//       version: "1.0.0",
+//       description: "API documentation for the RPG Tracker project"
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3000"
+//       },
+//       {
+//         url: 'https://rpgtracker.onrender.com/',
+//       }
+//     ]
+//   },
+//   apis: ["./routes/*.js"]
+// };
+
+// const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// app.get("/", (req, res) => {
+//   res.send("RPG Tracker API is running");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
