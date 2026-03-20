11c04cb8540441fa54f2049e30c99f047eaa3a16
Angela Murray <angelamurray776@outlook.com>
2026-03-19 22:25:45 -0400

    more swagger

--- swagger.js
+++ swagger.js
@@ -1,69 +1,22 @@
-// const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require('swagger-autogen')();
 
-// const doc = {
-//   info: {
-//     title: 'RPG Tracker API',
-//     description: 'API documentation for the RPG Tracker project'
-//   },
-//       servers: [
-//       {
-//         url: "http://localhost:3000"
-//       },
-//       {
-//         url: 'https://rpgtracker.onrender.com/',
-//       }
-//     ],
-// schemes: ['http', 'https'],
-// };
-
-// const outputFile = './swagger-output.json';
-// // const routes = ['./routes/characters.js', './routes/items.js', './routes/tasks.js', './routes/locations.js'];
-// const routes = ['./routes/index.js'];
-
-// swaggerAutogen(outputFile, routes, doc);
-
-// "npm run swagger" will rebuild the swagger-output file.
-
-
-
-const express = require("express");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-
-const app = express();
-app.use(express.json());
-
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "RPG Tracker API",
-      version: "1.0.0",
-      description: "API documentation for the RPG Tracker project"
-    },
-    servers: [
-      {
-        url: "http://localhost:3000"
-      },
-      {
-        url: 'https://rpgtracker.onrender.com/',
-      }
-    ]
+const doc = {
+  info: {
+    title: 'RPG Tracker API',
+    description: 'API documentation for the RPG Tracker project'
   },
-  apis: ["./routes/*.js"]
+      // host:['localhost:3000','rpgtracker.onrender.com'],
+    //   servers:
+    //    [
+    //   {
+    //     url: "http://localhost:3000"
+    //   },
+    //   {
+    //     url: 'https://rpgtracker.onrender.com/',
+    //   }
+    // ],
+schemes: ['http', 'https'],
 };
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.get("/", (req, res) => {
-  res.send("RPG Tracker API is running");
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
 
 const outputFile = './swagger-output.json';
 // const routes = ['./routes/characters.js', './routes/items.js', './routes/tasks.js', './routes/locations.js'];
@@ -72,3 +25,53 @@ const routes = ['./routes/index.js'];
 swaggerAutogen(outputFile, routes, doc);
 
 // "npm run swagger" will rebuild the swagger-output file.
+
+
+
+// const express = require("express");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerJsdoc = require("swagger-jsdoc");
+
+// const app = express();
+// app.use(express.json());
+
+// const swaggerOptions = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "RPG Tracker API",
+//       version: "1.0.0",
+//       description: "API documentation for the RPG Tracker project"
+//     },
+//     host: 'https://rpgtracker.onrender.com/'
+//     // [
+//     //   // {
+//     //   //   url: "http://localhost:3000"
+//     //   // },
+//     //   {
+//     //     url: 'https://rpgtracker.onrender.com/',
+//     //   }
+//     // ]
+//   },
+//   apis: ["./routes/*.js"]
+// };
+
+// const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// app.get("/", (req, res) => {
+//   res.send("RPG Tracker API is running");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+
+// const outputFile = './swagger-output.json';
+// // const routes = ['./routes/characters.js', './routes/items.js', './routes/tasks.js', './routes/locations.js'];
+// const routes = ['./routes/index.js'];
+
+// swaggerAutogen(outputFile, routes, doc);
+
+// // "npm run swagger" will rebuild the swagger-output file.
